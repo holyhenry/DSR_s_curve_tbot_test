6a682fa1121f13de0327c057eae8641396f2c1bd
Ashe Kim <ashekim@robotis.com>
2023-07-25 10:34:20 +0900

    update

--- tracking/pd_tracking_xy/src/pd_tracking_xy.py
+++ tracking/pd_tracking_xy/src/pd_tracking_xy.py
@@ -114,7 +114,7 @@ class PD:
         es = self.lowPass(es, self.es_last, lowPassGain=0.167)
 
         reinforce = self.v + self.beta*(es - self.es_last)/self.dt
-        reinforce = self.lowPass(reinforce, self.reinforce_last, lowPassGain=0.8)
+        reinforce = self.lowPass(reinforce, self.reinforce_last, lowPassGain=0.167)
         us_dot = self.alpha*es*self.beta + 1.0*(reinforce)
         # p_actual  = state[:2]
         # p_desired = goal[:2]
@@ -152,7 +152,7 @@ class PD:
         data.linear.x  = es
         data.linear.y  = ex
         data.linear.z  = ey
-        data.angular.x = reinforce
+        data.angular.x = 0.0
         data.angular.y = 0.0
         dataPub.publish(data)
 
