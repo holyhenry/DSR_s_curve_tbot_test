2d03b1926edda70f78d5307e4199ff0d12d95f0a
Henry Chang <henryc26@uw.edu>
2024-01-30 19:02:53 -0800

    udpate

--- lighting/scripts/square_neopixel.py
+++ lighting/scripts/square_neopixel.py
@@ -5,61 +5,71 @@ import rospy
 import numpy as np
 from geometry_msgs.msg import Twist
 
-angle = 0
+class lighting_node:
+    
+    def __init__(self) -> None:
 
-mode = 0
-mode_last = 0
+        self.bright = 0
+        self.angle = 0.0
 
-ns = rospy.get_namespace()
-bright = rospy.get_param(ns + "/lighting/brightness")
-pixels = neopixel.NeoPixel(board.D10, 64, brightness=bright)
-# pixels = neopixel.NeoPixel(board.D18, 64, brightness=bright)
+        self.mode = 0
+        self.mode_last = 0
 
-def angleDataCallback(msg):
+    def initNode(self):
 
-    angle = np.abs((msg.angular.z)*180/np.pi)
+        rospy.init_node('lighting', anonymous=True)
+        ns = rospy.get_namespace()
 
-def runLights():
+        self.bright = rospy.get_param(ns + "/lighting/brightness")
+        self.pixels = neopixel.NeoPixel(board.D10, 64, brightness=self.bright)
+        # pixels = neopixel.NeoPixel(board.D18, 64, brightness=bright)
 
-    smile_face   = [3,13,17,21,22,31,32,41,42,46,50,60]
-    cry_face     = [0,14,18,21,22,28,35,41,42,45,49,63]
-    neutral_face = [2,13,18,21,22,29,34,41,42,45,50,61]
+        rospy.Subscriber(ns + "data", Twist, self.angleDataCallback, queue_size=1)
 
-    if angle < 45:
-        mode = 1
-        if mode != mode_last:
-            pixels.fill((0,0,0))
-            print('mode change!') 
-        for i in smile_face:
-            pixels[i] = (0, 255, 0) # green
+    def angleDataCallback(self, msg):
 
-    elif 45 <= angle and angle < 60:
-        mode = 2
-        if mode != mode_last:
-            pixels.fill((0,0,0))
-            print('mode change!') 
-        for i in neutral_face:
-            pixels[i] = (255, 69, 0) # yellow
+        self.angle = np.abs((msg.angular.z)*180/np.pi)
 
-    else:
-        mode = 3
-        if mode != mode_last:
-            pixels.fill((0,0,0))
-            print('mode change!') 
-        for i in cry_face:
-            pixels[i] = (255, 0, 0) # red
+    def run(self):
 
-    mode_last = mode
+        self.initNode()
 
-def listener():
+        smile_face   = [3,13,17,21,22,31,32,41,42,46,50,60]
+        cry_face     = [0,14,18,21,22,28,35,41,42,45,49,63]
+        neutral_face = [2,13,18,21,22,29,34,41,42,45,50,61]
 
-    rospy.init_node('lighting', anonymous=True)
-    rospy.Subscriber(ns + "data", Twist, angleDataCallback, queue_size=1)
-    runLights()
-    rospy.spin()
+        if self.angle < 45:
+            self.mode = 1
+            if self.mode != self.mode_last:
+                self.pixels.fill((0,0,0))
+                print('mode change!') 
+            for i in smile_face:
+                self.pixels[i] = (0, 255, 0) # green
+
+        elif 45 <= self.angle and self.angle < 60:
+            self.mode = 2
+            if self.mode != self.mode_last:
+                self.pixels.fill((0,0,0))
+                print('mode change!') 
+            for i in neutral_face:
+                self.pixels[i] = (255, 69, 0) # yellow
+
+        else:
+            self.mode = 3
+            if self.mode != self.mode_last:
+                self.pixels.fill((0,0,0))
+                print('mode change!') 
+            for i in cry_face:
+                self.pixels[i] = (255, 0, 0) # red
+
+        self.mode_last = self.mode
+
+        if rospy.is_shutdown:
+            self.pixels.fill((0, 0, 0))
+
+        rospy.spin()
 
 if __name__ == '__main__':
 
-    listener()
-    if rospy.is_shutdown:
-        pixels.fill((0, 0, 0))
+    Lighting_node = lighting_node()
+    Lighting_node.run()
