5f053883ad48327fc93d3396ef923fca169afd58
Henry Chang <henryc26@uw.edu>
2024-06-07 16:15:02 -0700

    update

--- camera_multi_tag_2/scripts/cam_multi_tag_2.py
+++ camera_multi_tag_2/scripts/cam_multi_tag_2.py
@@ -30,11 +30,11 @@ class cam_multi_tag_node_2:
         self.pipeline = rs.pipeline()
         self.config   = rs.config()
 
-        self.w = 640
-        self.h = 360
+        self.w = 320
+        self.h = 240
 
         # Start streaming
-        self.config.enable_stream(rs.stream.color, self.h, self.w, rs.format.bgr8, 30)
+        self.config.enable_stream(rs.stream.color, self.w, self.h, rs.format.bgr8, 30)
         cfg = self.pipeline.start(self.config)
         self.cam_param = self.get_rs_param(cfg)
         print("Starting to stream")
