1b9ff1c570ecb5cacea178abc31aba35c4c8f1b5
Henry Chang <henryc26@uw.edu>
2025-04-29 18:57:59 -0700

    update tracking node

--- tracking/tracking/src/tracking_node.cpp
+++ tracking/tracking/src/tracking_node.cpp
@@ -90,7 +90,7 @@ void TrackingNode::aprilTagFilter()
         {   
             int id = static_cast<int>(tag_multi_raw_[i]);
             double x = tag_multi_raw_[i + 3];
-            double y = (tag_multi_raw_[i + 1] - cam_pos_offset);
+            double y = -(tag_multi_raw_[i + 1] - cam_pos_offset);
             double phi = tag_multi_raw_[i + 4];
 
             std::vector<double> infered = transformTag2Middle(x, y, phi, id);
@@ -115,8 +115,8 @@ void TrackingNode::aprilTagFilter()
             else
             {   // Log outlier detection
                 ROS_INFO_STREAM("Filtered tag ID:" << id);
-                ROS_INFO_STREAM("Filtered raw_xy at (" << x << ", " << y << ")");
-                ROS_INFO_STREAM("Filtered outlier infered_xy at (" << infered_x << ", " << infered_y << ")");
+                ROS_INFO_STREAM("Filtered raw_xy at (" << x << ", " << y << ")" << phi);
+                ROS_INFO_STREAM("Filtered outlier infered_xy at (" << infered_x << ", " << infered_y << ")", infered_phi);
             }
         }
     }
