1dc81ef2e960a5f4d9f07d5bd8a15f9acdea5827
Henry Chang <henryc26@uw.edu>
2025-10-21 18:07:12 -0700

    fix bug

--- tracking/tracking/src/tracking_node.cpp
+++ tracking/tracking/src/tracking_node.cpp
@@ -132,6 +132,8 @@ void TrackingNode::aprilTagFilter()
         }
     }
 
+    ROS_INFO_STREAM("GET TO HERE ++++++++++++++++++++++++++++++++++");
+
     if (count > 0)
     {
         tag_x /= count;
@@ -165,6 +167,8 @@ void TrackingNode::aprilTagFilter()
         // ROS_INFO_STREAM("tag_leader_state_: " << tag_leader_state_[0] << ", " << tag_leader_state_[1] << ")");
         // ROS_INFO_STREAM_THROTTLE(2.0, "global_leader_states_ size: " << global_leader_states_.rows());
     }
+
+    ROS_INFO_STREAM("GET TO HERE 9999999999999999999999999999999999999");
 }
 
 void TrackingNode::runControlStep()
