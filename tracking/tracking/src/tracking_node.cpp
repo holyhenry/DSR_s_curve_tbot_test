10ac8f87b66018f06f92478cfc27dc20c2ff63e3
Henry Chang <henryc26@uw.edu>
2025-11-20 15:56:14 -0800

    tmp

--- tracking/tracking/src/tracking_node.cpp
+++ tracking/tracking/src/tracking_node.cpp
@@ -186,10 +186,11 @@ void TrackingNode::aprilTagFilter()
 
         // Log current tag position
         ROS_INFO_STREAM("t_sec" << t_sec);
+        ROS_INFO_STREAM("movement" << movement);
         ROS_INFO_STREAM("tag_leader_state_: " << tag_leader_state_[0] << ", " << tag_leader_state_[1] << ")");
-        ROS_INFO_STREAM("global_leader_state: " << global_leader_state[0] << ", " << global_leader_state[1] << ")");
+        // ROS_INFO_STREAM("global_leader_state: " << global_leader_state[0] << ", " << global_leader_state[1] << ")");
         ROS_INFO_STREAM("global_leader_state_f: " << global_leader_state_f[0] << ", " << global_leader_state_f[1] << ")");
-        ROS_INFO_STREAM("global_leader_states_ size: " << global_leader_states_.size());
+        // ROS_INFO_STREAM("global_leader_states_ size: " << global_leader_states_.size());
     }
 }
 
