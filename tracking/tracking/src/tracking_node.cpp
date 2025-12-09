b6fd77ccc5ce10839b8a60681af38c58ee3d5015
Henry Chang <henryc26@uw.edu>
2025-12-09 12:10:49 -0800

    tmp

--- tracking/tracking/src/tracking_node.cpp
+++ tracking/tracking/src/tracking_node.cpp
@@ -212,7 +212,7 @@ void TrackingNode::runControlStep()
     controller_.setObservations(local_leader_states, cam_t_secs_);
 
     // 3. Compute the tracking target point
-    bool MEMORY_MODE = ture;
+    bool MEMORY_MODE = true;
     Eigen::Vector2d target = controller_.getTarget(MEMORY_MODE);
 
     // 4. Run control logic 
